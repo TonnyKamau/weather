35f6f34a151aae685922baeef2c2958f111c48cf
TonnyKamau <tonnykamau6@gmail.com>
2022-09-27 05:31:09 +0300

    setting up and usage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-
-import Search from './components/search/Search';
-import Currentweather from './components/currentweather/Currentweather'
+import Search from "./components/search/Search";
+import Currentweather from "./components/currentweather/Currentweather";
+import { weatherApiKey } from "./components/api/Api";
+import { useState } from "react";
 
 function App() {
-  const handleOnSearchChange=(searchData)=>
-  {
-console.log('')
-  }
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [weatherForecast, setWeatherForecast] = useState(null);
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherForecast = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
+    );
+    const weatherForecast = fetch(
+      `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
+    );
+    Promise.all([currentWeatherForecast, weatherForecast])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const foreCastResponse = await response[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setWeatherForecast({ city: searchData.label, ...foreCastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log(currentWeather);
+  console.log(weatherForecast);
   return (
     <div className="my-5 mx-auto max-w-screen-lg">
-     <Search onSearchChange={handleOnSearchChange}/>
-     <Currentweather />
+      <Search onSearchChange={handleOnSearchChange} />
+      <Currentweather />
     </div>
   );
 }
--- src/components/currentweather/Currentweather.js
+++ src/components/currentweather/Currentweather.js
@@ -1,19 +1,45 @@
-import React from 'react'
-
+import React from "react";
 
 const Currentweather = () => {
   return (
-    <div className='w-80 rounded-md shadow-2xl text-white bg-blue-700 mt-5 mx-auto mb-0'>
-      <div className=''>
-        <div>
-        <p className=''>Belgrade</p>
-        <p className=''>Sunny</p>
+    <div className="w-80 rounded-md shadow-2xl text-white mt-5 mx-auto mb-0 bg-slate-700 pt-0 pb-5 px-5">
+      <div className="flex justify-between items-center">
+        <div className="">
+          <p className="font-semibold text-xl leading-none m-0 tracking-wide">
+            Belgrade
+          </p>
+          <p className="text-sm leading-none m-0 font-normal">Sunny</p>
         </div>
-        <img src='icons/01d.png' alt='weather' className='' />
+        <img src="icons/01d.png" alt="weather" className="w-28" />
       </div>
-      
+      <div className="flex justify-between items-center">
+        <p className="font-semibold text-7xl w-auto tracking-tighter my-2,5 mx-0">
+          10°C
+        </p>
+        <div className="w-full pl-5">
+          <div className="flex justify-between">
+            <span className="text-left font-semibold text-xs">Details</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-left font-normal text-xs">Feels like</span>
+            <span className="text-right font-semibold text-xs">23°C</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-left font-normal text-xs">Wind</span>
+            <span className="text-right font-semibold text-xs">2 m/s</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-left font-normal text-xs">Humidity</span>
+            <span className="text-right font-semibold text-xs">10 %</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-left font-normal text-xs">Pressure</span>
+            <span className="text-right font-semibold text-xs" >23°C</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Currentweather
+export default Currentweather;
